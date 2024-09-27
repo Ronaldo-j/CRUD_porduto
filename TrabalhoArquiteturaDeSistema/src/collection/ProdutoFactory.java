393e44e8f66cef1741fcace6d2a12f585735c4d5
Ronaldo-j <128478364+Ronaldo-j@users.noreply.github.com>
2024-09-27 14:57:34 -0300

    Delete TrabalhoArquiteturaDeSistema directory

--- TrabalhoArquiteturaDeSistema/src/collection/ProdutoFactory.java
+++
@@ -1,6 +0,0 @@
-package collection;
-
-public interface ProdutoFactory {
-    Produto criarProduto(int id, String nome, String descricao, double preco, int quantidade);
-    
-}
--- TrabalhoArquiteturaDeSistema/src/collection/ProdutoFactoryImpl.java
+++
@@ -1,8 +0,0 @@
-package collection;
-
-public class ProdutoFactoryImpl implements ProdutoFactory {
-    public Produto criarProduto(int id, String nome, String descricao, double preco, int quantidade) {
-        return new Produto(id, nome, descricao, preco, quantidade);
-    }
-
-}
